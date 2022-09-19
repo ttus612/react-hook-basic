029a5a4cd7d41f7587dbc9ae0f7e3a5806c7818b
ThanhTu <thanhu6122002@gmail.com>
2022-09-19 16:56:24 +0700

    React Hooks useState(), ex toDoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.scss';
 import ColorBox from './components/ColorBox';
+import { useState } from 'react';
+import TodoList from './components/todoList';
 
 function App() {
+  const [toDoList, setToDoList] = useState([
+    { id: 1, title: 'I love Easy Fontend !😊'},
+    { id: 2, title: 'We love Easy Fontend ! 😍'},
+    { id: 3, title: 'They love Easy Fontend! 😘'}
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo)
+    const index = toDoList.findIndex(x => x.id === todo.id)
+    if (index < 0) {
+      return
+    }else{
+      const newToDoList = [...toDoList];
+      newToDoList.splice(index, 1)
+      setToDoList(newToDoList)
+    }
+  }
+
   return (
     <div className="app">
-      <h1>Welcome React Hooks</h1>
-
-      <ColorBox />
+      <h1>React Hook toDoList</h1>
+      <TodoList todos= {toDoList} onTodoClick= {handleTodoClick} />
     </div>
   );
 }
